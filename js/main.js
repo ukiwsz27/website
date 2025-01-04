7c4a6d2e9aa2a7a923b79fd2bf98d9dfbb3b4d08
andinugroho <137885674+ukiwsz27@users.noreply.github.com>
2025-01-04 20:40:32 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+---
+---
+
+{% include js/aos.js %}
+{% include js/xmlToJSON.js %}
+{% include js/markdown.js %}
+{% include js/detailsState.js %}
+{% include js/copyText.js %}
+{% include js/tooltips.js %}
+{% include js/scrollTo.js %}
+{% include js/base.js %}
+{% include js/updateLinkTargets.js %}
